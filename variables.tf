373ddafbc01c56d2fd6708186f1bc83b770243a0
amarnath <AmarnathGaneshkumar.Samantula@genpact.com>
2026-03-03 18:43:21 +0530

    Module

--- variables.tf
+++ variables.tf
@@ -1,30 +1,16 @@
 variable "resource_group_name" { type = string }
 variable "location" { type = string }
-
 variable "rg_name" { type = string }
-variable "location" { type = string }
 variable "address_space" { type = list(string) }
 variable "subnet_prefix" { type = string }
-
-variable "rg_name" { type = string }
-variable "location" { type = string }
-variable "subnet_id" { type = string }
-
-variable "rg_name" { type = string }
-variable "location" { type = string }
-
-
-variable "rg_name" { type = string }
-variable "location" { type = string }
 variable "subnet_id" { type = string }
 variable "avset_id" { type = string }
 variable "vm_count" { type = number }
 variable "vm_size" { type = string }
 variable "admin_username" { type = string }
 variable "admin_password" { type = string }
-
 variable "vm_ids" { type = list(string) }
-variable "vm_count" { type = number }
+
 
 
 
